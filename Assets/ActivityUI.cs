08a3758beb8c56fa52e852da2d7c41c559b9af43
Maciej Maj <maciejmaj@me.com>
2018-04-18 22:37:03 +0200

    Activity blocks, nesting activities

---
+++ Assets/ActivityUI.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ActivityUI : MonoBehaviour {
+
+	public GameObject place;
+	public Image move;
+	public Text Header;
+	public Text Subheader;
+	public Color[] activitesColor;
+
+	public ActivityType? type;
+	public int distance;
+	public float time;
+	public string placename;
+
+	public void Setup(ActivityType? type, int distance, float time, string placename = null) {
+		this.type = type;
+		this.distance = distance;
+		this.time = time;
+		this.placename = placename;
+
+		TimeSpan t = TimeSpan.FromSeconds(this.time);
+		SetSize(t);
+			
+		string timeShort = string.Format("{0}min ", t.Minutes);
+
+
+		if (type == null) {
+			place.SetActive(true);
+			move.gameObject.SetActive(false);
+			Header.text = placename;
+			Subheader.text = timeShort;
+			if (distance >= 100)
+				Subheader.text += distance.ToString() + "m";
+		} else {
+			place.SetActive(false);
+			move.gameObject.SetActive(true);
+			move.color = activitesColor[(int)type];
+			Header.gameObject.SetActive(false);
+			Subheader.text = timeShort + distance.ToString() + "m";
+		}
+	}
+
+	public void AddToExisting(int distance, float time) {
+		Debug.Log("Distance before: " + this.distance);
+		this.distance += distance;
+		Debug.Log("Distance after: " + this.distance);
+		this.time += time;
+
+		TimeSpan t = TimeSpan.FromSeconds(this.time);
+		SetSize(t);
+
+		string timeShort = string.Format("{0}min ", t.Minutes);
+		if (type == null) {
+			Subheader.text = timeShort;
+			if (distance >= 100)
+				Subheader.text += this.distance.ToString() + "m";
+		} else {
+			Subheader.text = timeShort + this.distance.ToString() + "m";
+		}
+	}
+
+	void SetSize(TimeSpan t) {
+		if (this.type == null) {
+			if (t.Minutes < 10)
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 60);
+			else if (t.Minutes < 30)
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 70);
+			else
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 80);
+		} else {
+			if (t.Minutes < 10)
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 30);
+			else if (t.Minutes < 30)
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 40);
+			else
+				GetComponent<RectTransform>().sizeDelta = new Vector2(0, 60);
+		}
+	}
+
+	public void DestroyActivity() {
+		ReadJson.instance.activitiesList.Remove(this);
+		Destroy(gameObject);
+	}
+}
--- Assets/ReadJson.cs
+++ Assets/ReadJson.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -66,22 +67,70 @@ public class MovesJson {
 }
 
 public class ReadJson : MonoBehaviour {
+	public static ReadJson instance;
+
+	public RectTransform historySpawn;
+	public GameObject activityPrefab;
+	public List<ActivityUI> activitiesList;
+
+	void Awake() {
+		instance = this;
+	}
 
 	void Start() {
 		TextAsset jsonData = Resources.Load("jsonstoryline2") as TextAsset;
 		MovesJson m = JsonConvert.DeserializeObject<MovesJson>(jsonData.text);
 		foreach (var item in m.segments) {
-			string output = item.type + "(";
-			int distance = 0;
-			foreach (var item2 in item.activities) {
-				distance += item2.distance;
-			}
-			if (item.type == "move") {
-				output += distance.ToString() + ")";
+			if (item.place == null) {
+				for (int i = 0; i < item.activities.Length; i++) {
+					SpawnActivity(item.activities[i].activity, item.activities[i].distance, item.activities[i].duration);
+				}
 			} else {
-				output += item.place.name + ", " + distance.ToString() + ")";
+				int distance = 0;
+				foreach (var item2 in item.activities) {
+					distance += item2.distance;
+				}
+				SpawnActivity(null, distance, CalculateTime(item.startTime, item.endTime), item.place.name);
 			}
-			Debug.Log(output);
 		}
+		ValidateIfNoReapeted();
+	}
+
+	float CalculateTime(string start, string end) {
+		//"endTime": "20180411T083834+0200",
+		DateTime a = new DateTime(
+			Convert.ToInt32(start.Substring(0,4)),
+			Convert.ToInt32(start.Substring(4, 2)),
+			Convert.ToInt32(start.Substring(6, 2)),
+			Convert.ToInt32(start.Substring(9, 2)),
+			Convert.ToInt32(start.Substring(11, 2)),
+			Convert.ToInt32(start.Substring(13, 2)));
+		DateTime b = new DateTime(
+			Convert.ToInt32(end.Substring(0, 4)),
+			Convert.ToInt32(end.Substring(4, 2)),
+			Convert.ToInt32(end.Substring(6, 2)),
+			Convert.ToInt32(end.Substring(9, 2)),
+			Convert.ToInt32(end.Substring(11, 2)),
+			Convert.ToInt32(end.Substring(13, 2)));
+		return (float)b.Subtract(a).TotalSeconds;
+	}
+	void ValidateIfNoReapeted() {
+		for (int i = 0; i < activitiesList.Count - 1; i++) {
+			if (activitiesList[i].type == activitiesList[i+1].type) {
+				if (activitiesList[i].type != null || (activitiesList[i].type == null && activitiesList[i].placename == activitiesList[i + 1].placename)) {
+					activitiesList[i].AddToExisting(activitiesList[i + 1].distance, activitiesList[i + 1].time);
+					activitiesList[i + 1].DestroyActivity();
+				}
+			}
+		}
+	}
+
+	void SpawnActivity(ActivityType? type, int distance, float time, string placeName = null) {
+		GameObject activity = Instantiate(activityPrefab, historySpawn.transform.position, historySpawn.transform.rotation);
+		RectTransform activityRect = activity.GetComponent<RectTransform>();
+		activity.transform.SetParent(historySpawn.transform);
+		activityRect.localScale = activityRect.lossyScale;
+		activitiesList.Add(activity.GetComponent<ActivityUI>());
+		activity.GetComponent<ActivityUI>().Setup(type, distance, time, placeName);
 	}
 }
