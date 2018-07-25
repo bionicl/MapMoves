69fe00fd979face562a1ab8da5a228ff1b15b018
Maciej Maj <maciejmaj@me.com>
2018-07-25 12:31:33 +0200

    Added MacOS Retina support

---
+++ Assets/CheckUIScale.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckUIScale : MonoBehaviour {
+
+	CanvasScaler canvas;
+
+	private void Awake() {
+		canvas = GetComponent<CanvasScaler>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (Screen.dpi >= 190)
+			canvas.scaleFactor = 2.6f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
